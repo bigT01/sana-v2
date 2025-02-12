50da9451a046c2124cc41636235e8ae53f025f17
tan <tanat8511@gmail.com>
2025-02-13 00:10:53 +0100

    feat: course page full

--- src/pages/courses/course/Course.tsx
+++ src/pages/courses/course/Course.tsx
@@ -2,6 +2,26 @@ import React from 'react';
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ExpandableTableOfContents from "../../../components/table/ExpandableTableOfContents";
+import BookIcon from '@mui/icons-material/Book';
+import {grey} from "@mui/material/colors";
+import {FiBook, FiShoppingBag} from "react-icons/fi";
+import {RiBarChart2Line} from "react-icons/ri";
+import List from "@mui/material/List";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItem from "@mui/material/ListItem";
+import {
+    FaCertificate,
+    FaClock,
+    FaClosedCaptioning, FaCommentDots,
+    FaFileAlt, FaGift,
+    FaLanguage,
+    FaMicrophone, FaShoppingBag,
+    FaUserGraduate
+} from "react-icons/fa";
+import ListItemText from "@mui/material/ListItemText";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
 
 
 const contents = [
@@ -58,7 +78,107 @@ const Course = () => {
                     </Typography>
                     <ExpandableTableOfContents title="COURSE TABLE OF CONTENTS" items={contents} />
                 </Box>
+                <Box content={'div'} sx={{width: '100%'}}>
+                    <Box content={'div'} sx={{width: '100%', height: '48px', boxShadow: '0px 5px 16px #00000010', borderRadius: '1rem', display: 'flex', mb: 3, border: `1px solid ${grey[400]}`}}>
+                        <Box content={'div'} sx={{width: '50%', display: 'flex', pl: '0.5rem', gap: 2, alignItems:'center', borderRight: `1px solid ${grey[400]}`}}>
+                            <FiBook size={24}/>
+                            <Box content={'div'} sx={{ display:'flex', flexDirection: 'column', gap: 0.1}}>
+                                <Typography component="span" variant="body2" sx={{opacity: 0.5}} fontWeight={400}>
+                                    Lesson
+                                </Typography>
+                                <Typography component="span" variant="body2" fontWeight={550}>
+                                    12
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box content={'div'} sx={{width: '50%', display: 'flex', pl: '0.5rem', gap: 2, alignItems:'center'}}>
+                            <RiBarChart2Line size={24}/>
+                            <Box content={'div'} sx={{ display:'flex', flexDirection: 'column', gap: 0.1}}>
+                                <Typography component="span" variant="body2" sx={{opacity: 0.5}} fontWeight={400}>
+                                    Difficulty
+                                </Typography>
+                                <Typography component="span" variant="body2" fontWeight={550}>
+                                    Moderate
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Stack spacing={2} mb={4}>
 
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaUserGraduate size={18}/>
+                            <Typography fontSize={16}>Students: <strong>3,215</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaLanguage size={18}/>
+                            <Typography fontSize={16}>Language: <strong>English</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="start" spacing={1}>
+                            <FaClosedCaptioning size={18}/>
+                            <Typography fontSize={16}>Subtitles: <strong>English, Spanish, French, Italian, Russian, Polish, Dutch, German</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaFileAlt size={18}/>
+                            <Typography fontSize={16}>Additional resources: <strong>12 files</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaClock size={18}/>
+                            <Typography fontSize={16}>Duration: <strong>8h 23m</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaCommentDots size={18}/>
+                            <Typography fontSize={16}>Critique session: <strong>Individual recordings</strong></Typography>
+                        </Stack>
+
+                        <Stack direction="row" alignItems="center" spacing={1}>
+                            <FaCertificate size={18}/>
+                            <Typography fontSize={16}>Certificate: <strong>Upon completion of the course</strong></Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack direction="row" spacing={2} mb={4}>
+                        <Button sx={{
+                            bgcolor: (theme) => theme.palette.mode === 'dark' ? '#FFFFFF' :'#000000',
+                            width: '45%'
+                        }} variant="contained" startIcon={<FiShoppingBag />}>
+                            Enroll a course
+                        </Button>
+
+                        <Button sx={{
+                            // borderColor: (theme) => theme.palette.mode === 'dark' ? '#FFFFFF' :'#000000',
+                            width: '45%'
+                        }} variant="outlined" startIcon={<FiShoppingBag />}>
+                            Buy as a Gift
+                        </Button>
+                    </Stack>
+                    <Divider sx={{mb: 4}}/>
+                    <Box sx={{ maxWidth: '90%', lineHeight: 1.6 }}>
+                        <Typography variant="subtitle2" sx={{ fontWeight: "medium", color: "gray", mb: 1 }}>
+                            ASSIGNMENT
+                        </Typography>
+                        <Typography variant="body1" sx={{ mb: 3 }}>
+                            Plan to dedicate a minimum of 1-2 hours per day to watch lecture videos, engage in Q&A sessions, and complete assignments.
+                        </Typography>
+
+                        <Typography variant="subtitle2" sx={{ fontWeight: "medium", color: "gray", mb: 1 }}>
+                            PREREQUISITES
+                        </Typography>
+                        <Typography variant="body1" sx={{ mb: 3 }}>
+                            Familiarity with Blender’s user interface and navigation, basic manipulation of SOPs (Surface Operators), and understanding of fundamental mathematical concepts related to vectors are recommended.
+                        </Typography>
+
+                        <Typography variant="subtitle2" sx={{ fontWeight: "medium", color: "gray", mb: 1 }}>
+                            MATERIALS
+                        </Typography>
+                        <Typography variant="body1">
+                            Blender 2.93 (or higher) | Recommended system requirements: 8-core processor and 32GB of RAM. To participate in this workshop, you can download the free educational edition of Blender.
+                        </Typography>
+                    </Box>
+                </Box>
             </Box>
 
         </Box>
