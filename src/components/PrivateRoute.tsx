0afd3abc516085421e71b595f12843d9fc905e7a
tan <tanat8511@gmail.com>
2025-04-20 01:23:35 +0200

    fix: authorization main route

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Navigate, Outlet} from 'react-router-dom';
 import {useStore} from "../store/useStore";
 import {IState} from "../constants/interfaces";
@@ -8,12 +8,30 @@ interface PrivateRouteProps {
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({authenticationPath}) => {
-    const token = useStore((state: IState) => state.token)
+    const getProfile = useStore((state: IState) => state.getProfile);
+    const token = useStore((state: IState) => state.token); 
+
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            const response = await getProfile();
+            
+            setIsLoaded(true);
+        };
+
+        fetchProfile();
+    }
+    , []);
+    
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    }
     if (!token) {
-        return <Navigate to={authenticationPath} replace/>;
+        return <Navigate to={authenticationPath} />;
     }
 
-    return <Outlet/>;
+    return  <Outlet/>;
 };
 
 export default PrivateRoute;
